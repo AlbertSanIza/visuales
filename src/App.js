9737b0cb67b695829cc48b6496905396b183bf97
Albert Sanchez <albert.san.iza@gmail.com>
2018-07-24 09:57:09 -0700

    moved header to its own component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Header from './header/Header.js'
 import Footer from './footer/Footer.js'
 import './App.css'
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div>
+            <Header/>
             <Footer/>
             </div>
         )
---
+++ src/header/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './Header.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="row">
+            <div className="col-12">
+            <h1 className="display-3">Visuales</h1>
+            <hr/>
+            </div>
+            </div>
+        )
+    }
+}
+
+export default Header
