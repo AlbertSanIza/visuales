fa6c482a93a9761b404f18bfbc30b6849dcd4af0
Albert Sanchez <albert.san.iza@gmail.com>
2018-07-23 23:29:43 -0700

    mooved footer to its own folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import Footer from './Footer.js'
+import Footer from './footer/Footer.js'
 import './App.css'
 
 class App extends Component {
