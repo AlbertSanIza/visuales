7a0a5c5f25cdcc6c933aad0e9ddbedc02abb3660
Albert Sanchez <albert.san.iza@gmail.com>
2018-07-23 22:52:23 -0700

    cleared app

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import './App.css'
 class App extends Component {
     render() {
         return (
-            <div className="App">
-            <h1>Hola Mundo</h1>
-            <div class="alert alert-primary" role="alert">
-  This is a primary alert—check it out!
-</div>
-            </div>
+            <div className="App"></div>
         )
     }
 }
