eb89a5f248eb6c4e4d160c3ff9ea5c95f85cb2d0
Albert Sanchez <albert.san.iza@gmail.com>
2018-07-24 17:56:43 -0700

    created a visualizer

---
+++ src/visualizer/Visualizer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Visualizer extends Component {
+    render() {
+        return (
+            <div>
+            I am the Visualizer
+            </div>
+        )
+    }
+}
+
+export default Visualizer
