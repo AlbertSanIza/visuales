cf296fb5969717244bdffbdbc94bdd8a12b16969
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-04 16:03:31 -0700

    smaller header

--- src/header/Header.js
+++ src/header/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return (
             <div>
-            <h1 className="display-3">Visuales</h1>
+            <h1 className="display-4">Visuales</h1>
             <hr/>
             </div>
         )
